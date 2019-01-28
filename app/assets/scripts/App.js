15329a90748e76ace0a0402cf189705a8ac3c482
ringuralte <p4ringa@gmail.com>
2019-01-28 13:22:00 +0530

    finished reveal-on-scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.getElementsByClassName('feature-item'), '85%');
+new RevealOnScroll(document.getElementsByClassName('testimonial'), '90%');
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,34 @@
+import waypoints from '../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    for(let i=0; i<this.itemsToReveal.length; i++) {
+      let item = this.itemsToReveal[i];
+      item.classList.add('reveal-item');
+    }
+  }
+
+  createWaypoints() {
+    let that = this;
+    for(let i=0; i<this.itemsToReveal.length; i++) {
+      let currentItem = this.itemsToReveal[i];
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          currentItem.classList.add('reveal-item--is-visible');
+        },
+        offset: that.offsetPercentage
+      });
+    }
+  }
+
+}
+
+export default RevealOnScroll;
