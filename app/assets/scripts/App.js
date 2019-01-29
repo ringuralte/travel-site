966db530e6a34ebd74dceffce7617fdcbf32c3a0
ringuralte <p4ringa@gmail.com>
2019-01-29 14:47:31 +0530

    didnt complete it cause i cant get a module to work without jquery

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
+import StickyHeader from './modules/StickyHeader';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.getElementsByClassName('feature-item'), '85%');
-new RevealOnScroll(document.getElementsByClassName('testimonial'), '90%');
+new RevealOnScroll(document.getElementsByClassName('testimonial'), '95%');
+
+let stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,27 @@
+import waypoints from '../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class StickyHeader {
+  constructor() {
+    this.siteHeader = document.getElementsByClassName('site-header');
+    this.headerTriggerElement = document.getElementsByClassName('large-hero__title');
+    this.createHeaderWayPoint();
+  }
+
+  createHeaderWayPoint() {
+    let header = this.siteHeader[0];
+    let trigger = this.headerTriggerElement[0];
+    new Waypoint({
+      element: trigger,
+      handler: function(direction) {
+        if(direction == "down") {
+          header.classList.add('site-header--dark');
+        } else {
+          header.classList.remove('site-header--dark');
+        }
+      },
+    });
+  }
+
+}
+
+export default StickyHeader;
